080146cbbb35c855001b0c755c7dc9c3d71e35e5
caella03 <caellaisimbi28@gmail.com>
2026-05-13 12:02:41 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("Product Registration Form Loaded");
-});
+function login(){
+
+  let email = document.getElementById("email").value;
+
+  let password = document.getElementById("password").value;
+
+  if(email === "admin@gmail.com" && password === "1234"){
+
+    document.getElementById("message").innerHTML =
+    "Someone logged in!";
+
+    console.log("User logged in");
+
+  }
+
+  else{
+
+    document.getElementById("message").innerHTML =
+    "Wrong email or password";
+
+  }
+
+}
